6e84d4c3f5fca645c6b27881f4fb8d59e1fdc1cb
Ramsteinz <lol@cgocable.ca>
2014-09-18 13:49:18 -0400

    Create Project-X - Yasuo.lua

---
+++ Project-X - Yasuo.lua
@@ -0,0 +1,46 @@
+--------------------------------------------------------
+--                Project-X Present
+--          
+--                      Yasuo
+--              
+--                  by Ramsteinz
+--------------------------------------------------------
+if myHero.charName ~= "Yasuo" then return end
+
+--------------------------------------------------------
+--  Update Libs and main Script
+--------------------------------------------------------
+local version = "1.00"
+local DOWLOADING_LIBS, DOWNLOAD_COUNT = false, 0
+local UPDATE_NAME = "Project-X - Yasuo"
+
+
+-- called once when the script is loaded
+function OnLoad()
+
+end
+
+-- handles script logic, a pure high speed loop
+function OnTick()
+
+end
+
+--handles overlay drawing (processing is not recommended here,use onTick() for that)
+function OnDraw()
+
+end
+
+--handles input
+function OnWndMsg(msg,key)
+
+end
+
+-- listens to chat input
+function OnSendChat(txt)
+
+end
+
+-- listens to spell
+function OnProcessSpell(owner,spell)
+
+end
